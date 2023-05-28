cf9871ed9b0b4eee6ac95b1bee4fccc8f25c1c91
Pramo-d <promody1444@gmail.com>
2023-05-28 12:41:29 +0530

    react pratice

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -6,7 +6,7 @@ function ExpenseItem(){
             <h2>Food Rs 10</h2>
             <h2>Petrol Rs 100</h2>
             <h2>movie Rs 200</h2>
-        </div>
+          </div>
     );
 }
 export default ExpenseItem;
