c1045c94f513956f5cb5e03740cc560a4ea2d7df
Pramo-d <promody1444@gmail.com>
2023-05-28 18:32:01 +0530

    here LocationOfexpenditure who track money spend

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,11 +1,20 @@
+ import './ExpenseItem.css';
 
 function ExpenseItem(){
+    const ExpenseDate=new Date(2023,4,28 );
+    const ExpenseTittle='Car Insurance';
+    const ExpensePrice= "$400";
+    const ExpenseLocation='LocationOfExpenditure'
     return (
-        <div>
-            <h1>Expense Item </h1>
-            <h2>Food Rs 10</h2>
-            <h2>Petrol Rs 100</h2>
-            <h2>movie Rs 200</h2>
+        <div className="expense-item">
+             <div> {ExpenseDate.toISOString()}</div>
+             <div className="expense-item__description"> 
+               
+                <h2> {ExpenseLocation}:  {ExpenseTittle}</h2>
+                <div className="expense-item__price"> {ExpensePrice}</div>
+              </div>
+               
+
           </div>
     );
 }
