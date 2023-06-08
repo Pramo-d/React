db627eec5802b3bf9fa8efcf2533fa70ba9ea222
Pramo-d <promody1444@gmail.com>
2023-06-08 17:07:45 +0530

    some sample click operation work without state

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -5,17 +5,17 @@ import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const title=props.title;
+  const amount=props.amount;
   const clickHandler=()=>{
-       title='update';
-        console.log(title);
+        amount=0;
+        console.log(amount);
   }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2> {title}</h2>
-        <div className="expense-item__price"> ${props.amount}</div>
+        <h2> {props.title}</h2>
+        <div className="expense-item__price"> ${amount}</div>
       </div>
       <button onClick={clickHandler}>Delete Expense</button>
     </Card>
