c1084edf7b46a4c568627bff54dcbb812622991b
Pramo-d <promody1444@gmail.com>
2023-06-08 17:03:12 +0530

    practice with onClick button

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -4,16 +4,22 @@ import React from "react";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
-const ExpenseItem=(props)=> {
+const ExpenseItem = (props) => {
+  const title=props.title;
+  const clickHandler=()=>{
+       title='update';
+        console.log(title);
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2> {props.title}</h2>
+        <h2> {title}</h2>
         <div className="expense-item__price"> ${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Delete Expense</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
